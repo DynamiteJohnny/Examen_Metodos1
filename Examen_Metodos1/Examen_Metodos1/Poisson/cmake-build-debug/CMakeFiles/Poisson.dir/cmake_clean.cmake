e2e0b03ec6e4d5d0fd5881b72090fbbd58e35559
DynamiteJohnny <a01261426@itesm.mx>
2018-02-25 15:28:27 -0600

    Suarez

---
+++ Examen_Metodos1/Examen_Metodos1/Poisson/cmake-build-debug/CMakeFiles/Poisson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poisson.dir/main.c.obj"
+  "Poisson.pdb"
+  "Poisson.exe"
+  "Poisson.exe.manifest"
+  "libPoisson.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Poisson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
