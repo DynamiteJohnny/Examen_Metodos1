739bcfe5981675f33c94845ca4a93d0eb64fdf1f
Sharatgm <A01205254@itesm.mx>
2018-03-15 08:01:59 -0600

    Modified reject method

--- Numeros aleatorios/reject.py
+++ Numeros aleatorios/reject.py
@@ -7,10 +7,17 @@ def fun1(val):
 
 def fun2(val):
     res = 0
-    if (val > 6):
-        res = 1.3333 - (val/6)
-    elif (val > 2):
+    if (val <= 6 and val >= 2):
+        #print("Entro")
         res = -0.666 + (val/12)
+        #print("res = %f" % (res))
+    else:
+        if (val > 6 and val <= 8):
+            res = 1.3333 - (val/6)
+            #print("res = %f" % (res))
+        else:
+            res = 0
+            #print("res = %f" % (res))
     return res
 
 
@@ -25,16 +32,18 @@ def varianza(ls,media):
     return b*acumulator
 
 
+
 def method():
 
     a = int(input("Limite inferior:\n"))
     b = int(input("Limite superior:\n"))
     n = int(input("Introduce n:\n"))
-    m = int(input("Introduce m:\n"))
+    m = float(input("Introduce m:\n"))
+
 
     numbers = []
 
-    """num = int(input("\t1. Función 1 (2x)\n\t2. Función 2 (-1/6 + x/12 | 4/3 - x/6)\nElige una opción: "))"""
+    num = int(input("\t1. Función 1 (2x)\n\t2. Función 2 (-1/6 + x/12 | 4/3 - x/6)\nElige una opción: "))
 
 
     while True:
@@ -43,19 +52,26 @@ def method():
         else:
             r1 = random()
             r2 = random()
+            #print("R1= %f\n R2 = %f" % (r1,r2))
+            #r1 = float(input("Introduce r1:\n"))
+            #r2 = float(input("Introduce r2:\n"))
 
             x_star = a + (b - a) * r1
-            f_star = fun1(x_star)
+            if num == 1:
+                f_star = fun1(x_star)
+            else:
+                f_star = fun2(x_star)
 
             if (r2 <= (f_star/m)):
-                numbers.append(x_star)
+                numbers.append(x_star - 3)
 
 
-    media = sum(numbers) / float(len(numbers))
+    media = sum(numbers) / len(numbers)
     print("Numeros:")
     print (numbers)
     print ("\nMedia: %f" % (media))
     print ("Varianza: %f " % (varianza(numbers,media)))
+    print ("X mínima : %f" % (min(numbers)))
     print ("X máxima : %f" % (max(numbers)))
 
 
