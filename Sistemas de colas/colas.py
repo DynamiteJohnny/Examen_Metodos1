f882491f34b160c0738b82054e9e33676df55d6f
Sharatgm <A01205254@itesm.mx>
2018-03-21 17:14:50 -0600

    Little rescued and M/M/1 finished

--- Sistemas de colas/colas.py
+++ Sistemas de colas/colas.py
@@ -10,6 +10,38 @@ import math
 
 print("Métodos Cuantitativos y simulación\nAlumnos:\n\tShara Teresa González Mena\n\tEsteban Quintana Cueto\n\tJuan Luis Suarez Ruiz\n\n")
 
+def little():
+-    print("\n***** Ley de little *****")
+-
+-    lam = float(input("Introduce el valor de lambda(Tasa media de llegadas) = "))
+-    miu = float(input("Introduce el valor de Miu(Numero esperado de clientes en cola) = "))
+-
+-    gv = float(input("Seleccione el valor que le dan: \n\t1)Wq\n\t2)Ws\n\t3)Ls\n\t4)Lq"))
+-
+-    if gv == 1:
+-        wq = float(input("Introduce el valor de Wq(Tiempo esperado de espera en la cola)="))
+-        lq = lam*wq
+-        ws = wq+(1/miu)
+-        ls = lam*ws
+-    elif gv == 2:
+-        ws = float(input("Introduce el valor de Ws(Tiempo esperado de espera en el sistema)="))
+-        ls = lam*ws
+-        wq = ws-(1/miu)
+-        lq = lam*wq
+-    elif gv == 3:
+-        ls = float(input("Introduce el valor de Ls(Número esperado de clientes en el sistema)="))
+-        ws = ls/lam
+-        wq = ws-(1/miu)
+-        lq = lam*wq
+-    elif gv == 4:
+-        lq = float(input("Introduce el valor de Lq(Número esperado de clientes en la cola)="))
+-        wq = lq/lam
+-        ls = lq+(lam/miu)
+-        lq = lam*wq
+-    else:
+-        print("Opción invalida")
+-
+-    print("\n\tLs = %f\tLq = %f\tWs = %f\tWq = %f"%(ls,lq,ws,wq))
 
 def mm1():
     print("\n Modelo M/M/1")
@@ -48,12 +80,13 @@ def mm1():
     return 0
 
 def mms():
-    
+
     return 0
-options = { 1 : mm1,
-            2 : mms
+options = { 1 : little,
+            2 : mm1,
+            3 : mms
 	   }
 
-num = int(input("SISTEMAS DE COLAS:\n\t1. M/M/1\n\t2. M/M/S\n\nElige una opción: "))
+num = int(input("SISTEMAS DE COLAS:\n\t1. Little\n\t2. M/M/1\n\t3. M/M/S\n\nElige una opción: "))
 
 options[num]()
