16b090840c0e24f4bfd930563b31dfc326437ec1
Sharatgm <A01205254@itesm.mx>
2018-03-21 23:42:30 -0600

    Fixed little and MMS

--- Sistemas de colas/colas.py
+++ Sistemas de colas/colas.py
@@ -11,41 +11,50 @@ import math
 print("Métodos Cuantitativos y simulación\nAlumnos:\n\tShara Teresa González Mena\n\tEsteban Quintana Cueto\n\tJuan Luis Suarez Ruiz\n\n")
 
 def little():
--    print("\n***** Ley de little *****")
--
--    lam = float(input("Introduce el valor de lambda(Tasa media de llegadas) = "))
--    miu = float(input("Introduce el valor de Miu(Numero esperado de clientes en cola) = "))
--
--    gv = float(input("Seleccione el valor que le dan: \n\t1)Wq\n\t2)Ws\n\t3)Ls\n\t4)Lq"))
--
--    if gv == 1:
--        wq = float(input("Introduce el valor de Wq(Tiempo esperado de espera en la cola)="))
--        lq = lam*wq
--        ws = wq+(1/miu)
--        ls = lam*ws
--    elif gv == 2:
--        ws = float(input("Introduce el valor de Ws(Tiempo esperado de espera en el sistema)="))
--        ls = lam*ws
--        wq = ws-(1/miu)
--        lq = lam*wq
--    elif gv == 3:
--        ls = float(input("Introduce el valor de Ls(Número esperado de clientes en el sistema)="))
--        ws = ls/lam
--        wq = ws-(1/miu)
--        lq = lam*wq
--    elif gv == 4:
--        lq = float(input("Introduce el valor de Lq(Número esperado de clientes en la cola)="))
--        wq = lq/lam
--        ls = lq+(lam/miu)
--        lq = lam*wq
--    else:
--        print("Opción invalida")
--
--    print("\n\tLs = %f\tLq = %f\tWs = %f\tWq = %f"%(ls,lq,ws,wq))
+    print("\nLEY DE LITTLE\n")
+    lam = float(input("Valor de lambda(Tasa media de llegadas) = "))
+    miu = float(input("Valor de Miu(Numero esperado de clientes en cola) = "))
 
+    op = int(input("Elige una opción: \n\t1. Calcular las medidas de desempeño\n\t2. Factor de utilización con uno o más servidores\n>> "))
+
+    if(op == 1):
+        gv = float(input("\nValor dado en el problema: \n\t1)Wq\n\t2)Ws\n\t3)Ls\n\t4)Lq\n>>"))
+
+        if gv == 1:
+            wq = float(input("Valor de Wq(Tiempo esperado de espera en la cola)="))
+            lq = lam*wq
+            ws = wq+(1/miu)
+            ls = lam*ws
+        elif gv == 2:
+            ws = float(input("Valor de Ws(Tiempo esperado de espera en el sistema)="))
+            ls = lam*ws
+            wq = ws-(1/miu)
+            lq = lam*wq
+        elif gv == 3:
+            ls = float(input("Valor de Ls(Número esperado de clientes en el sistema)="))
+            ws = ls/lam
+            wq = ws-(1/miu)
+            lq = lam*wq
+        elif gv == 4:
+            lq = float(input("Valor de Lq(Número esperado de clientes en la cola)="))
+            wq = lq/lam
+            ls = lq+(lam/miu)
+            lq = lam*wq
+        else:
+            print("Opción invalida")
+
+        print("\n\tLs = %f\tLq = %f\tWs = %f\tWq = %f"%(ls,lq,ws,wq))
+
+    else:
+        serv = int(input("\nCantidad de servidores: "))
+        ro = lam / serv * miu
+        print("\nro con %d servidores = %f\n" % (serv, ro))
+
+
+    return 0
 
 def mm1():
-    print("\n Modelo M/M/1")
+    print("\n MODELO M/M/1\n")
 
     lam = float(input("Lambda (Tasa media de llegadas) = "))
     miu = float(input("Miu (Tasa media de servicio) = "))
@@ -86,37 +95,36 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
-def p0(lam,miu,ro,s):
+def p0sum(lam,miu,s,ro):
 	sum = 0
 	for n in range(0,s):
- 		sum = sum + ((math.pow((lam/miu),n)/factorial(n))+((math.pow((lam/miu),s))/(factorial(s)*(1 - ro))))
-	
-	return (1/sum)
+ 		sum = sum + ((math.pow((lam/miu),n)/factorial(n)))
+
+	return (1/(sum+((math.pow((lam/miu),s))/(factorial(s)*(1 - ro)))))
 
 def mms():
-	print("\n Modelo M/M/1")
+    print("\n MODELO M/M/1")
 
     lam = float(input("Lambda (Tasa media de llegadas) = "))
     miu = float(input("Miu (Tasa media de servicio) = "))
-    s = float(input("Numero de servideores: "))
+    s = int(input("Numero de servidores: "))
 
     ro = lam/(s*miu)
+    print("ro = ", ro)
 
-    p0 = p0(lam,miu,s,ro)
-
+    p0 = p0sum(lam,miu,s,ro)
     lq = ((math.pow(lam/miu,s))*p0*ro)/(factorial(s)*math.pow(1 - ro,2))
     wq = lq/lam
-    ws = wq + (1/lam)
+    ws = wq + (1/miu)
     ls = lam*ws
 
     print("\nMedidas de desempeño del sistema: \n\tLs = %f\tLq = %f\tWs = %f\tWq = %f"%(ls,lq,ws,wq))
-    
-    n = float(input("La probabilidad que desea calcular: "))
-
-    pn = (math.pow(lam/miu,n))*p0/(factorial(n))
-
-    print("\nProbabilidad: W%i = %f"%(n,pn))
-
+    op = 1
+    while op == 1:
+        n = float(input("\nLa probabilidad que desea calcular: "))
+        pn = (math.pow(lam/miu,n))*p0/(factorial(n))
+        print("\nProbabilidad: W%i = %f"%(n,pn))
+        op = int(input("\n¿Desea calcular otra probabilidad?\n\t1. Si\n\t2. No\n>>"))
 
 
     return 0
