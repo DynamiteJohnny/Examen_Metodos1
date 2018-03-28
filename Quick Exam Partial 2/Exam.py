6d317d483a33116bc849b55f2b1ac41e649080ca
DynamiteJohnny <a01261426@itesm.mx>
2018-03-27 22:28:05 -0600

    error en min

--- Quick Exam Partial 2/Exam.py
+++ Quick Exam Partial 2/Exam.py
@@ -20,7 +20,7 @@ p = (n*n)*(porcentaje/100)
 round(p,0)
 
 p = int(p)
-print("\nP = %i\n"% i)
+print("\nP = %i\n"% p)
 
 for i in range(p):
 	r1 = r2 = 0
@@ -29,7 +29,20 @@ for i in range(p):
 		r2 = randint(0, n-1)
 	Matrix[r1][r2] = 1
 
+nummax = 0
+nummin = n-1
+cont = 0
 for i in range(n):
+	if cont > nummax:
+		nummax = cont
+	if cont < nummin and cont > 0:
+		nummin = cont
+	cont = 0
 	for j in range(n):
+		if Matrix[i][j] == 1:
+			cont = cont + 1
 		print(Matrix[i][j], end = " ")
 	print()
+
+
+print("Max = %i\tMin = %i"%(nummax,nummin))
