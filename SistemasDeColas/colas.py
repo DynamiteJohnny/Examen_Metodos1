00975cb3381ef4d69f64e7575f499326c50f314b
Sharatgm <A01205254@itesm.mx>
2018-04-04 21:33:47 -0500

    Added print(ro) to M/M/1

--- SistemasDeColas/colas.py
+++ SistemasDeColas/colas.py
@@ -64,13 +64,14 @@ def mm1():
     lq = pow(lam,2) / (miu * (miu - lam))
     ws = 1 / (miu - lam)
     wq = lam / (miu * (miu - lam))
-
+    ro = lam/miu
+    print("\n\tro (Factor de utilización) = ", ro)
     print("\nMedidas de desempeño del sistema: \n\tLs = %f\tLq = %f\tWs = %f\tWq = %f"%(ls,lq,ws,wq))
     op = 0
     while(op != 5):
         print("\nProbabilidades\n\t1. Pn (Probabilidad de tener n clientes)\n\t2. P(Ls > n)\n\t3. P(Ws > t)\n\t4. P(Wq > t)\n\t5. Salir")
         op = int(input("\n¿Qué probabilidad deseas calcular? "))
-        ro = lam/miu
+
 
         if op == 1:
             n = int(input("Valor de n = "))
